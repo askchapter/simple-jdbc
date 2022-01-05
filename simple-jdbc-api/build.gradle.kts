e04a7810c038e2f89f79d98f990c24c4de297719
Robert Fidler <robertf224@gmail.com>
2022-01-05 00:21:04 -0500

    fix typescript publishing

--- simple-jdbc-api/build.gradle.kts
+++ simple-jdbc-api/build.gradle.kts
@@ -1,1 +1,18 @@
+import com.palantir.gradle.conjure.api.ConjureExtension
+import com.palantir.gradle.conjure.api.GeneratorOptions
+import groovy.lang.Closure
+
 apply(plugin = "com.palantir.conjure")
+
+tasks.named("publishTypeScript") {
+    doFirst {
+        file("src/.npmrc").writeText("//registry.npmjs.org/:_authToken=${System.getenv()["NPM_TOKEN"]}")
+    }
+}
+
+configure<ConjureExtension> {
+    typescript(delegateClosureOf<GeneratorOptions> {
+        setProperty("nodeCompatibleModules", true)
+    } as Closure<GeneratorOptions>)
+}
+
